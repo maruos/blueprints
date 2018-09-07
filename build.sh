3aa4921215b0263d4827cd7afa0f58485fc04425
Preetam D'Souza <preetamjdsouza@gmail.com>
2018-09-07 11:45:12 -0400

    trivial: Clean up build script
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>

--- build.sh
+++ build.sh
@@ -28,27 +28,26 @@ set -u
 
 readonly MARU_TAG="maru-g$(git rev-parse --short HEAD)"
 
-print_help () {
+OPT_BLUEPRINT="debian"
+OPT_NAME="$OPT_BLUEPRINT"
+
+help () {
     cat <<EOF
 Build container images for Maru OS
 
 Usage: build.sh [OPTIONS]
 
     -b, --blueprint     Blueprint to use, currently only 'debian'.
-                        Defaults to debian.
+                        Defaults to '$OPT_BLUEPRINT'.
 
     -n, --name          Optional container name. Defaults to blueprint name.
                         This is used to set /etc/hostname in the rootfs.
 EOF
 }
 
-mecho () {
-    echo "--> ${1}"
-}
-
 mount_binfmts () {
     if ! mount | grep -q 'binfmt_misc' ; then
-        mecho "enabling binfmts for cross-bootstrapping..."
+        echo "[*] Enabling binfmts for cross-bootstrapping..."
         mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
     fi
     update-binfmts --enable
@@ -59,30 +58,20 @@ cleanup () {
     local rootfs_dir="$2"
     local tmp_dir="$3"
 
-    mecho "cleaning up..."
+    echo "[*] Cleaning up..."
     blueprint_cleanup "$opt_name" "$rootfs_dir"
     if [ -d "$tmp_dir" ] ; then
         rm -rf "$tmp_dir"
     fi
 }
 
-OPT_BLUEPRINT="debian"
-OPT_NAME="$OPT_BLUEPRINT"
-
-ARGS="$(getopt -o b:n:h --long blueprint:,name:,help -n 'build.sh' -- "$@")"
-if [ $? != 0 ] ; then
-    echo >&2 "Error parsing options!"
-    exit 2
-fi
-
-eval set -- "$ARGS"
-
-while true; do
-    case "$1" in
+while [ $# -gt 0 ]; do
+    case $1 in
         -b|--blueprint) OPT_BLUEPRINT="$2"; shift 2 ;;
         -n|--name) OPT_NAME="$2"; shift 2 ;;
-        -h|--help) print_help; exit 0 ;;
+        -h|--help) help; exit 0 ;;
         --) shift; break ;;
+        *) echo >&2 "[x] Unknown option '$1'"; help; exit 2 ;;
     esac
 done
 
@@ -93,24 +82,24 @@ readonly ROOTFS_TAR="${OUT_DIR}/${MARU_TAG}-${OPT_NAME}-rootfs.tar.gz"
 
 plugin="$(pwd)/blueprint/${OPT_BLUEPRINT}/plugin.sh"
 if [ ! -e "$plugin" ] ; then
-    echo >&2 "${OPT_BLUEPRINT} isn't a valid blueprint!"
+    echo >&2 "[x] $OPT_BLUEPRINT isn't a valid blueprint!"
     exit 2
 fi
 
 # prep for cross-bootstrapping
 mount_binfmts
 
-mecho "loading distro plugin..."
+echo "[*] Loading distro plugin..."
 pushd >/dev/null "$(dirname "$plugin")"
 
 source "$plugin"
 
 trap "cleanup $OPT_NAME $ROOTFS_DIR $TMP_DIR" EXIT
 
-mecho "building image..."
+echo "[*] Building image..."
 blueprint_build "$OPT_NAME" "$ROOTFS_DIR" "$@"
 
-mecho "creating a rootfs compressed archive..."
+echo "[*] Creating a rootfs compressed archive..."
 tar czf "$ROOTFS_TAR" -C "$(dirname "$ROOTFS_DIR")" "$(basename "$ROOTFS_DIR")"
 
 # prepend hash to release for sanity checks
@@ -118,4 +107,4 @@ sha1="$(sha1sum "$ROOTFS_TAR" | cut -c -8)"
 release="${ROOTFS_TAR%.tar.gz}-${sha1}.tar.gz"
 mv "$ROOTFS_TAR" "$release"
 
-mecho "Build success! See $release"
+echo "[*] All tasks completed successfully. See '$release'."
