4f70c33fa7c20e6f3a34a32e417b614a5dc47cbf
Preetam D'Souza <preetamjdsouza@gmail.com>
2019-02-15 09:34:11 -0500

    build: Mount /var/cache in Docker
    I accidentally deleted the /var/cache volume mount in my previous
    commit, woops.
    
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -7,5 +7,6 @@ if [ $# -gt 0 ]; then parameters="$@"; fi
 
 docker build -t maruos/blueprints .
 docker run --privileged --rm \
+        -v /var/cache:/var/cache \
         -v "$(pwd)":/var/maru \
         -ti maruos/blueprints ${parameters}
